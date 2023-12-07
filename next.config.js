3ba4220f79fdc3915d70a350564640ada06339cf
wangtz <wangtz@tiens.com>
2023-12-07 19:57:08 +0800

    hotfix

--- next.config.js
+++ next.config.js
@@ -3,4 +3,12 @@ module.exports = {
   images: {
     domains: ["picsum.photos"],
   },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/api/:path*',
+  //       destination: 'https://tidb-cloud-starter-five-omega.vercel.app', // 将此处的 example.com 替换为实际需要代理的域名
+  //     },
+  //   ]
+  // }, 
 };
